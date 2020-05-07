be7ea0108e6a1c270ec7b53f9691d7354fb279ab
shakyaprasen <kaji.prasen@gmail.com>
2020-05-07 16:39:16 +0545

    Added babel-cli and build script

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,7 @@ var cors = require('cors')
 const app = express();
 app.use(cors())
 
-const port = process.env.PORT || 5000;
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/home', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED' });
-});
+const port = 80;
 
 app.get('/test', (req, res) => {
   res.json({
@@ -20,3 +12,11 @@ app.get('/test', (req, res) => {
     }
   });
 });
+
+// create a GET route
+app.get('*', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED' });
+});
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
