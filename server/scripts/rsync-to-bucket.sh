150ee351eff5742a5a6cedb7811128c0b8649412
shakyaprasen <kaji.prasen@gmail.com>
2020-05-09 13:31:51 +0545

    changes to rsync script

--- server/scripts/rsync-to-bucket.sh
+++ server/scripts/rsync-to-bucket.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gsutil -m rsync -r ./storage/ gs://place-kitten-bucket/storage/
+PATH=$PATH:/snap/bin
+GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
+gsutil -m rsync -r /home/kajiprasen/place-kitten/server/storage/ gs://place-kitten-bucket/storage/
