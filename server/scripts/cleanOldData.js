596b436210bf83a62ba997d6aa296e517b8e2cd3
shakyaprasen <kaji.prasen@gmail.com>
2020-05-11 20:35:27 +0545

    handled error case

--- server/scripts/cleanOldData.js
+++ server/scripts/cleanOldData.js
@@ -9,14 +9,14 @@ fs.readdir(directory, (err, files) => {
   files.forEach((file) => {
     const filePath = path.resolve(directory, file);
     fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) throw err;
+      if (err) console.log(err);
       const obj = JSON.parse(data);
       if (obj.time >= Date.now() - (5 * 60 * 60 * 1000)) { //delete files older than 5 hrs
         return;
       }
       const imagePath = path.resolve(__dirname, obj.dir, obj.name);
       fs.unlink(imagePath, (err) => {
-        if (err) throw err;
+        if (err) console.log(err);
         console.log('image deleted', imagePath);
         // fs.unlink(filePath, (err) => {
         //   if (err) throw err;
