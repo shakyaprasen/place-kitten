e04116bebeac3d40bdda01e2856b2b3594d9a911
shakyaprasen <kaji.prasen@gmail.com>
2020-05-12 10:05:57 +0545

    fixed rsync script

--- server/scripts/rsync-to-bucket.sh
+++ server/scripts/rsync-to-bucket.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/snap/bin
-GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
-gsutil -m rsync -r /home/kajiprasen/place-kitten/server/storage/ gs://place-kitten-bucket/storage/
+
+/snap/bin/gcloud auth activate-service-account --key-file /home/kajiprasen/.secrets/Kittens-fbcec4490df3.json
+/snap/bin/gsutil -m rsync -r /home/kajiprasen/place-kitten/server/storage/ gs://place-kitten-bucket/
+
