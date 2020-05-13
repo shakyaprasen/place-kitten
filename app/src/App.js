241679e9174285c5d0e693b9e3c9e68568e0d873
shakyaprasen <kaji.prasen@gmail.com>
2020-05-13 11:37:31 +0545

    Added dotenv to frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const BASE_URL = '35.186.144.89:8080';
 //url fixed for now
-axios.get(`${BASE_URL}/api/images`)
+axios.get(`${process.env.BASE_URL}/api/images`)
   .then(({ data }) => {
     const files = data.data.files;
     let imageHtml = '';
     const imageDiv = document.getElementById('root-image');
 
     files.forEach((file) => {
-      imageHtml+= `<img class="image-file" src="${BASE_URL}/images/${file}"></img>`;
+      imageHtml+= `<img class="image-file" src="${process.env.BASE_URL}/images/${file}"></img>`;
     });
     imageDiv.innerHTML = imageHtml;
   });
