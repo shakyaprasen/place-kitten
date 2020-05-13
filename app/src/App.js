2525162d2bbec64467908e8e38a4ed54efd0daee
shakyaprasen <kaji.prasen@gmail.com>
2020-05-13 11:35:36 +0545

    Changed url of app to reflect server url

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
+const BASE_URL = '35.186.144.89:8080';
 //url fixed for now
-axios.get("http://localhost:8080/api/images")
+axios.get(`${BASE_URL}/api/images`)
   .then(({ data }) => {
     const files = data.data.files;
     let imageHtml = '';
     const imageDiv = document.getElementById('root-image');
 
     files.forEach((file) => {
-      imageHtml+= `<img class="image-file" src="http://localhost:8080/images/${file}"></img>`;
+      imageHtml+= `<img class="image-file" src="${BASE_URL}/images/${file}"></img>`;
     });
     imageDiv.innerHTML = imageHtml;
   });
